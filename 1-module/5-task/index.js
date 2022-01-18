a9cb5ca9f3bfc6396d9ede0782619349413d6646
LaNadya <estelhawke@gmail.com>
2022-01-18 19:42:27 +0400

    1 module

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,10 @@
 function truncate(str, maxlength) {
-  // ваш код...
-}
+  a = str.length;
+  if ( a > maxlength) {
+    str = str.slice(0, (maxlength - 1));
+    str = str.slice(0) + "…";
+    return str;
+  } else {
+    return str;
+  }
+};
