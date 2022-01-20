546b3cd40c2c3d15b290a1503e7f16e6c59b2c1d
LaNadya <estelhawke@gmail.com>
2022-01-20 21:00:25 +0400

    2 module

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,18 @@
 let calculator = {
-  // ваш код
-};
+
+  read (a, b) {
+  calculator.firstNum = a;
+  calculator.secondNum = b;
+  },
+  sum() {
+  let sum = this.firstNum + this.secondNum;
+  return sum;
+  },
+  mul() {
+  let mul = this.firstNum * this.secondNum;
+  return mul;
+  }
+  };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
